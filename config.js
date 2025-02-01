f10661e0a4258a326e116f92e4779945870bf3f6
Shaun <regancipher@gmail.com>
2025-02-01 22:05:49 +0000

    Small amendments to footer #5

--- config.js
+++ config.js
@@ -1,97 +1,124 @@
 // Configuration options
-const init_phones = ["Harman 2019 IE v2 Target.txt"],            // Optional. Which graphs to display on initial load. Note: Share URLs will override this set
-      DIR = "data/",                                // Directory where graph files are stored
-      default_channels = ["L","R"],                 // Which channels to display. Avoid javascript errors if loading just one channel per phone
-      default_normalization = "dB",                 // Sets default graph normalization mode. Accepts "dB" or "Hz"
-      default_norm_db = 60,                         // Sets default dB normalization point
-      default_norm_hz = 500,                        // Sets default Hz normalization point (500Hz is recommended by IEC)
-      max_channel_imbalance = 5,                    // Channel imbalance threshold to show ! in the channel selector
-      alt_layout = true,                           // Toggle between classic and alt layouts
-      alt_sticky_graph = true,                      // If active graphs overflows the viewport, does the graph scroll with the page or stick to the viewport?
-      alt_animated = true,                         // Determines if new graphs are drawn with a 1-second animation, or appear instantly
-      alt_header = false,                           // Display a configurable header at the top of the alt layout
-      alt_header_new_tab = true,                    // Clicking alt_header links opens in new tab
-      alt_tutorial = true,                         // Display a configurable frequency response guide below the graph
-      alt_augment = true,                          // Display augment card in phone list, e.g. review sore, shop link
-      site_url = '',                      // URL of your graph "homepage"
-      share_url = true,                             // If true, enables shareable URLs
-      watermark_text = "",                 		// Optional. Watermark appears behind graphs
-      watermark_image_url = "",   // Optional. If image file is in same directory as config, can be just the filename
+const init_phones = ["Harman 2019 IE v2 Target.txt"],
+      DIR = "data/",
+      default_channels = ["L", "R"],
+      default_normalization = "dB",
+      default_norm_db = 60,
+      default_norm_hz = 500,
+      max_channel_imbalance = 5,
+      alt_layout = true,
+      alt_sticky_graph = true,
+      alt_animated = true,
+      alt_header = false,
+      alt_header_new_tab = true,
+      alt_tutorial = true,
+      alt_augment = true,
+      site_url = "",
+      share_url = true,
+      watermark_text = "",
+      watermark_image_url = "",
       page_title = "Frequency Responses - ReganCipher",
       page_description = "View and compare frequency response graphs for earphones",
-      accessories = true,                          // If true, displays specified HTML at the bottom of the page. Configure further below
-      externalLinksBar = false,                      // If true, displays row of pill-shaped links at the bottom of the page. Configure further below
-      restricted = false,                           // Enables restricted mode. More restricted options below
-      expandable = false,                           // Enables button to expand iframe over the top of the parent page
-      expandableOnly = false,                       // Prevents iframe interactions unless the user has expanded it. Accepts "true" or "false" OR a pixel value; if pixel value, that is used as the maximum width at which expandableOnly is used
-      headerHeight = '0px',                         // Optional. If expandable=true, determines how much space to leave for the parent page header
-      darkModeButton = true,                        // Adds a "Dark Mode" button the main toolbar to let users set preference
-      targetDashed = true,                         // If true, makes target curves dashed lines
-      targetColorCustom = false,                    // If false, targets appear as a random gray value. Can replace with a fixed color value to make all targets the specified color, e.g. "black"
-      labelsPosition = "bottom-left",                   // Up to four labels will be grouped in a specified corner. Accepts "top-left," bottom-left," "bottom-right," and "default"
-      stickyLabels = true,                          // "Sticky" labels 
-      analyticsEnabled = true,                     // Enables Google Analytics 4 measurement of site usage
-      exportableGraphs = true,                      // Enables export graph button
-      extraEnabled = true,                          // Enable extra features
-      extraUploadEnabled = true,                    // Enable upload function
-      extraEQEnabled = true,                        // Enable parametic eq function
-      extraEQBands = 10,                            // Default EQ bands available
-      extraEQBandsMax = 20,                         // Max EQ bands available
-      extraToneGeneratorEnabled = true;             // Enable tone generator function
+      accessories = true,
+      externalLinksBar = false,
+      restricted = false,
+      expandable = false,
+      expandableOnly = false,
+      headerHeight = "0px",
+      darkModeButton = true,
+      targetDashed = true,
+      targetColorCustom = false,
+      labelsPosition = "bottom-left",
+      stickyLabels = true,
+      analyticsEnabled = true,
+      exportableGraphs = true,
+      extraEnabled = true,
+      extraUploadEnabled = true,
+      extraEQEnabled = true,
+      extraEQBands = 10,
+      extraEQBandsMax = 20,
+      extraToneGeneratorEnabled = true;
 
 // Specify which targets to display
 const targets = [
-    { type:"Neutral",    files:["Diffuse Field","Etymotic","Free Field","Innerfidelity ID"] },
-{ type:"Preference", files:["Harman 2019 IE v2", "Harman 2018 OE", "Rtings","Sonarworks"] },
-    { type:"Reviewer",   files:["Antdroid","Bad Guy","Banbeucmas","Crinacle","Precogvision","Super Review"] }
+    { 
+        type: "Neutral",
+        files: ["Diffuse Field", "Etymotic", "Free Field", "Innerfidelity ID"]
+    },
+    { 
+        type: "Preference",
+        files: ["Harman 2019 IE v2", "Harman 2018 OE", "Rtings", "Sonarworks"]
+    },
+    { 
+        type: "Reviewer",
+        files: ["Antdroid", "Bad Guy", "Banbeucmas", "Crinacle", "Precogvision", "Super Review"]
+    }
 ];
 
-
-
-// *************************************************************
-// Functions to support config options set above; probably don't need to change these
-// *************************************************************
-
-// Set up the watermark, based on config options above
+// Functions to support config options
 function watermark(svg) {
     let wm = svg.append("g")
-        .attr("transform", "translate("+(pad.l+W/2)+","+(pad.t+H/2-20)+")")
-        .attr("opacity",0.2);
+        .attr("transform", "translate(" + (pad.l + W/2) + "," + (pad.t + H/2 - 20) + ")")
+        .attr("opacity", 0.2);
     
-    if ( watermark_image_url ) {
+    if (watermark_image_url) {
         wm.append("image")
-            .attrs({x:-64, y:-64, width:128, height:128, "xlink:href":watermark_image_url});
+            .attrs({
+                x: -64,
+                y: -64,
+                width: 128,
+                height: 128,
+                "xlink:href": watermark_image_url
+            });
     }
     
-    if ( watermark_text ) {
+    if (watermark_text) {
         wm.append("text")
-            .attrs({x:0, y:70, "font-size":28, "text-anchor":"middle", "class":"graph-name"})
+            .attrs({
+                x: 0,
+                y: 70,
+                "font-size": 28,
+                "text-anchor": "middle",
+                "class": "graph-name"
+            })
             .text(watermark_text);
     }
     
     let wmSq = svg.append("g")
-        .attr("opacity",0.2);
+        .attr("opacity", 0.2);
     
     wmSq.append("image")
-        .attrs({x:481, y:152, width:150, height:141, "class":"wm-squiglink-logo", "xlink:href":"squiglink-giggle.svg"});
+        .attrs({
+            x: 481,
+            y: 152,
+            width: 150,
+            height: 141,
+            "class": "wm-squiglink-logo",
+            "xlink:href": "squiglink-giggle.svg"
+        });
     
     wmSq.append("text")
-        .attrs({x:641, y:314, "font-size":10, "transform":"translate(0,0)", "text-anchor":"end", "class":"wm-squiglink-address"})
+        .attrs({
+            x: 641,
+            y: 314,
+            "font-size": 10,
+            "transform": "translate(0,0)",
+            "text-anchor": "end",
+            "class": "wm-squiglink-address"
+        })
         .text("regancipher.squig.link");
-
 }
 
-
-
-// Parse fr text data from REW or AudioTool format with whatever separator
+// Parse fr text data
 function tsvParse(fr) {
     return fr.split(/[\r\n]/)
-        .map(l => l.trim()).filter(l => l && l[0] !== '*')
+        .map(l => l.trim())
+        .filter(l => l && l[0] !== '*')
         .map(l => l.split(/[\s,]+/).map(e => parseFloat(e)).slice(0, 2))
         .filter(t => !isNaN(t[0]) && !isNaN(t[1]));
 }
 
-// Apply stylesheet based layout options above
+// Apply stylesheet based layout options
 function setLayout() {
     function applyStylesheet(styleSheet) {
         var docHead = document.querySelector("head"),
@@ -99,12 +126,11 @@ function setLayout() {
         
         linkTag.setAttribute("rel", "stylesheet");
         linkTag.setAttribute("type", "text/css");
-        
         linkTag.setAttribute("href", styleSheet);
         docHead.append(linkTag);
     }
 
-    if ( !alt_layout ) {
+    if (!alt_layout) {
         applyStylesheet("style.css");
     } else {
         applyStylesheet("style-alt.css");
@@ -113,11 +139,9 @@ function setLayout() {
 }
 setLayout();
 
-
-
 // Set restricted mode
 function setRestricted() {
-    if ( restricted ) {
+    if (restricted) {
         max_compare = 2;
         restrict_target = false;
         disallow_target = true;
@@ -126,10 +150,7 @@ function setRestricted() {
 }
 setRestricted();
 
-
-
-// Configure HTML accessories to appear at the bottom of the page. Displayed only if accessories (above) is true
-// There are a few templates here for ease of use / examples, but these variables accept any HTML
+// Configure HTML accessories
 const paragraphs = `
     <h2>Welcome!</h2>
 
@@ -152,90 +173,28 @@ const paragraphs = `
     </ul>
 `;
 
-    // Customize the count of widget divs, and customize the contents of them. As long as they're wrapped in the widget div, they should auto-wrap and maintain margins between themselves
-    widgets = `
-        <div class="accessories-widgets">
-            <div class="widget">
-                <img width="200" src="cringraph-logo.svg"/>
-            </div>
-
-            <div class="widget">
-                <img width="200" src="cringraph-logo.svg"/>
-            </div>
-
-            <div class="widget">
-                <img width="200" src="cringraph-logo.svg"/>
-            </div>
+const widgets = `
+    <div class="accessories-widgets">
+        <div class="widget">
+            <img width="200" src="cringraph-logo.svg"/>
         </div>
-    `,
-    // Which of the above variables to actually insert into the page
-    whichAccessoriesToUse = simpleAbout;
 
+        <div class="widget">
+            <img width="200" src="cringraph-logo.svg"/>
+        </div>
 
+        <div class="widget">
+            <img width="200" src="cringraph-logo.svg"/>
+        </div>
+    </div>
+`;
 
-// Configure external links to appear at the bottom of the page. Displayed only if externalLinksBar (above) is true
-const linkSets = [
-    {
-        label: "IEM graph databases",
-        links: [
-            {
-                name: "Audio Discourse",
-                url: "https://iems.audiodiscourse.com/"
-            },
-            {
-                name: "Bad Guy",
-                url: "https://hbb.squig.link/"
-            },
-            {
-                name: "Banbeucmas",
-                url: "https://banbeu.com/graph/tool/"
-            },
-            {
-                name: "HypetheSonics",
-                url: "https://www.hypethesonics.com/iemdbc/"
-            },
-            {
-                name: "In-Ear Fidelity",
-                url: "https://crinacle.com/graphs/iems/graphtool/"
-            },
-            {
-                name: "Precogvision",
-                url: "https://precog.squig.link/"
-            },
-            {
-                name: "Rikudou Goku",
-                url: "https://rg.squig.link/"
-            },
-            {
-                name: "Super* Review",
-                url: "https://squig.link/"
-            },
-        ]
-    },
-    {
-        label: "Headphones",
-        links: [
-            {
-                name: "Audio Discourse",
-                url: "https://headphones.audiodiscourse.com/"
-            },
-            {
-                name: "In-Ear Fidelity",
-                url: "https://crinacle.com/graphs/headphones/graphtool/"
-            },
-            {
-                name: "Super* Review",
-                url: "https://squig.link/hp.html"
-            }
-        ]
-    }
-];
-
-
+// Set which accessories to use
+const whichAccessoriesToUse = paragraphs;
 
 // Set up analytics
 function setupGraphAnalytics() {
-    if ( analyticsEnabled ) {
+    if (analyticsEnabled) {
         const pageHead = document.querySelector("head"),
               graphAnalytics = document.createElement("script"),
               graphAnalyticsSrc = "graphAnalytics.js";
@@ -246,66 +205,63 @@ function setupGraphAnalytics() {
 }
 setupGraphAnalytics();
 
-
-
-// If alt_header is enabled, these are the items added to the header
-let headerLogoText = "",
-    headerLogoImgUrl = "rcheader.png",
-    headerLinks = [
+// Header configuration
+const headerLogoText = "",
+      headerLogoImgUrl = "rcheader.png",
+      headerLinks = [
     {
         name: "YouTube",
         url: "https://www.youtube.com/channel/UCH6h7agGcLNqqw_zV8DMD5Q"
     },
-{
+    {
         name: "Headfi",
         url: "https://www.head-fi.org/members/regancipher.535074/#showcase-reviews"
     },
-{
+    {
         name: "Reddit",
         url: "https://www.reddit.com/user/Radiant-Cherry-7973/?rdt=57804"
     },
-{
+    {
         name: "Instagram",
         url: "https://www.instagram.com/regancipher/?hl=gb"
     }
-
 ];
 
-
-let tutorialDefinitions = [
+// Tutorial definitions
+const tutorialDefinitions = [
     {
-        name: 'Sub bass',
-        width: '20.1%',
-        description: 'Lorem ipsum.'
+        name: "Sub bass",
+        width: "20.1%",
+        description: "Lorem ipsum."
     },
     {
-        name: 'Mid bass',
-        width: '19.2%',
-        description: 'Lorem ipsum.'
+        name: "Mid bass",
+        width: "19.2%",
+        description: "Lorem ipsum."
     },
     {
-        name: 'Lower midrange',
-        width: '17.4%',
-        description: 'Lorem ipsum.'
+        name: "Lower midrange",
+        width: "17.4%",
+        description: "Lorem ipsum."
     },
     {
-        name: 'Upper midrange',
+        name: "Upper midrange",
         width: "20%",
-        description: 'Lorem ipsum.'
+        description: "Lorem ipsum."
     },
     {
-        name: 'Presence region',
-        width: '6%',
-        description: 'Lorem ipsum.'
+        name: "Presence region",
+        width: "6%",
+        description: "Lorem ipsum."
     },
     {
-        name: 'Mid treble',
-        width: '7.3%',
-        description: 'Lorem ipsum.'
+        name: "Mid treble",
+        width: "7.3%",
+        description: "Lorem ipsum."
     },
     {
-        name: 'Air',
-        width: '10%',
-        description: 'Lorem ipsum.'
+        name: "Air",
+        width: "10%",
+        description: "Lorem ipsum."
     }
-]
+];
